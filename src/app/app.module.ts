a8a74fd2557e91855c7e95e3bca47d4881259c1c
Marcelo <marcelo.ungaretti@gmail.com>
2022-01-31 21:46:41 -0300

    curso concluido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DisableControlModule } from './shared/components/yes-no-button-group/directives/disable-control/disable-control.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     YesNoButtonGroupModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    DisableControlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
